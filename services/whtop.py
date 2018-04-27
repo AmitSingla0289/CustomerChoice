34d810100359d91020c5491e34822b2fe8ec0321
madhvi.gupta <meetmadhvi@gmail.com>
2018-04-27 11:18:58 +0530

    "Whtop corrected ratings"

--- services/whtop.py
+++ services/whtop.py
@@ -16,14 +16,14 @@ class whtop():
         authors = response.xpath("//div[@property='author']/span[1]/text()").extract()
         dates = response.xpath("//div[@class='review-date']/time/text()").extract()
         website_name = response.xpath("//div[@id='line']/a[1]/img/@alt").extract()
-        ratings1 = response.xpath("//div[@class='review-rating']/span[@property='ratingValue']/@content").extract()
+        ratings = response.xpath("//div[@class='review-rating']/span[@property='ratingValue']/@content").extract()
         for node in response.xpath("//div[@class='review-content']"):
             reviews.append(node.xpath('string()').extract());
         if len(reviews) == 0:
             for node in response.xpath('//div[@class="comment pure-u-1 pure-u-lg-2-3 wcc"]'):
                 reviews.append(node.xpath('string()').extract());
         for item in range(0, len(reviews)):
-            servicename1 = ServiceRecord(response.url, None, None, dates[item], authors[item], category,
+            servicename1 = ServiceRecord(response.url, ratings[item], None, dates[item], authors[item], category,
                           servicename, reviews[item], None, website_name);
             servicename1.save()
 
